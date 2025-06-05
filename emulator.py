6c41058cf9cacc5605d10a420982af0c40c0da91
Eliana Mari Lim <lim.eliana.mari@gmail.com>
2025-06-05 17:54:07 +0800

    feat: arch242isa

--- emulator.py
+++ emulator.py
@@ -53,7 +53,270 @@ class Arch242Emulator:
     def process_instruction(self, instr: int):
         pass
 
+class Arch242ISA:
+    def init(self, emulator: Arch242Emulator):
+        self.emu = emulator
 
+    def rotr(self):
+        self.emu.acc = ((self.emu.acc >> 1) | ((self.emu.acc & 0x01) << 3)) & 0xF
+        self.emu.pc += 1
+
+    def rotl(self):
+        self.emu.acc = ((self.emu.acc << 1) | ((self.emu.acc >> 3) & 0x1)) & 0xF
+        self.emu.pc += 1
+
+    def rotrc(self):
+        temp = self.emu.cf
+        self.emu.cf = self.emu.acc & 0x01
+        self.emu.acc = (self.emu.acc >> 1) | (temp << 3) & 0xF
+        self.emu.pc += 1
+
+    def rotlc(self):
+        temp = self.emu.cf
+        self.emu.cf = self.emu.acc >> 3 & 0x01
+        self.emu.acc = ((self.emu.acc << 1) | temp) & 0xF
+        self.emu.pc += 1
+
+    def frommba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.acc = self.emu.data_mem[addr] & 0x0F
+        self.emu.pc += 1
+
+    def tomba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.data_mem[addr] = self.emu.acc
+        self.emu.pc += 1
+
+    def frommdc(self):
+        addr = self.emu.reg[3] << 4 | self.emu.reg[2]
+        self.emu.acc = self.emu.data_mem[addr] & 0x0F
+        self.emu.pc += 1
+
+    def tomdc(self):
+        addr = self.emu.reg[3] << 4 | self.emu.reg[2]
+        self.emu.data_mem[addr] = self.emu.acc
+        self.emu.pc += 1
+
+    def addcmba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.acc += self.emu.data_mem[addr] + self.emu.cf
+        self.emu.cf = 1 if self.emu.acc & 0x0F else 0
+        self.emu.acc &= 0x0F
+        self.emu.pc += 1
+
+    def addmba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.acc += self.emu.data_mem[addr]
+        self.emu.cf = 1 if self.emu.acc & 0x0F else 0
+        self.emu.acc &= 0x0F
+        self.emu.pc += 1
+
+    def subcmba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.acc = self.emu.acc - self.emu.data_mem[addr] + self.emu.cf
+        self.emu.cf = 1 if self.emu.acc < 0 else 0
+        self.emu.acc &= 0x0F
+        self.emu.pc += 1
+
+    def submba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.acc = self.emu.acc - self.emu.data_mem[addr]
+        self.emu.cf = 1 if self.emu.acc < 0 else 0
+        self.emu.acc &= 0x0F
+        self.emu.pc += 1
+
+    def incmba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.data_mem[addr] += 1
+        self.emu.pc += 1
+
+    def decmba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.data_mem[addr] -= 1
+        self.emu.pc += 1
+
+    def incmdc(self):
+        addr = self.emu.reg[3] << 4 | self.emu.reg[2]
+        self.emu.data_mem[addr] += 1
+        self.emu.pc += 1
+
+    def decmdc(self):
+        addr = self.emu.reg[3] << 4 | self.emu.reg[2]
+        self.emu.data_mem[addr] -= 1
+        self.emu.pc += 1
+
+    def increg(self):
+        curr_instr = self.emu.instr_mem[self.emu.pc]
+        curr_reg = curr_instr & 0xE >> 1
+        self.emu.reg[curr_reg] += 1
+        self.emu.pc += 1
+
+    def decreg(self):
+        curr_instr = self.emu.instr_mem[self.emu.pc]
+        curr_reg = curr_instr & 0x0E >> 1
+        self.emu.reg[curr_reg] -= 1
+        self.emu.pc += 1
+
+    def andba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.acc = (self.emu.acc & self.emu.data_mem[addr]) & 0x0F
+        self.emu.pc += 1
+
+    def xorba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.acc = (self.emu.acc ^ self.emu.data_mem[addr]) & 0x0F
+        self.emu.pc += 1
+
+    def orba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.acc = (self.emu.acc | self.emu.data_mem[addr]) & 0x0F
+        self.emu.pc += 1
+
+    def andmba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.data_mem[addr] = (self.emu.acc | self.emu.data_mem[addr]) & 0x0F
+        self.emu.pc += 1
+
+    def xormba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.data_mem[addr] = self.emu.acc ^ self.emu.data_mem[addr]
+        self.emu.pc += 1
+
+    def ormba(self):
+        addr = self.emu.reg[1] << 4 | self.emu.reg[0]
+        self.emu.data_mem[addr] = self.emu.acc | self.emu.data_mem[addr]
+        self.emu.pc += 1
+
+    def toreg(self):
+        curr_reg = (self.emu.instr_mem[self.emu.pc] & 0xe) >> 1
+        self.emu.reg[curr_reg] = self.emu.acc
+        self.emu.pc += 1
+
+    def fromreg(self):
+        curr_reg = (self.emu.instr_mem[self.emu.pc] & 0xe) >> 1
+        self.emu.acc = self.emu.reg[curr_reg]
+        self.emu.pc += 1
+
+    def clrcf(self):
+        self.emu.cf = 0
+        self.emu.pc += 1
+
+    def setcf(self):
+        self.emu.cf = 1
+        self.emu.pc += 1
+
+    def ret(self):
+        self.emu.pc = (self.emu.pc & 0xF000) | (self.emu.temp & 0x0FFF)
+        self.temp = 0
+
+    def fromioa(self):
+        self.emu.acc = self.emu.ioa
+        self.emu.pc += 1
+
+    def inc(self):
+        self.emu.acc += 1
+        self.emu.pc += 1
+
+    def bcd(self):
+        if self.emu.acc >= 10 or self.emu.cf == 1:
+            self.emu.acc += 6
+            self.emu.acc &= 0xF
+            self.emu.cf = 1
+        self.emu.pc += 1
+
+    def shutdown(self):
+        pyxel.quit()
+
+    def nop(self):
+        pass
+
+    def dec(self):
+        self.emu.acc -= 1
+        self.emu.pc += 1
+
+    def addimm(self):
+        self.emu.acc = self.emu.acc + (self.emu.instr_mem[self.emu.pc+1] & 0x0F)
+        self.emu.pc += 2
+
+    def subimm(self):
+        self.emu.acc = self.emu.acc - (self.emu.instr_mem[self.emu.pc+1] & 0x0F)
+        self.emu.pc += 2
+    
+    def andimm(self):
+        self.emu.acc = self.emu.acc & (self.emu.instr_mem[self.emu.pc+1] & 0x0F)
+        self.emu.pc += 2
+
+    def xorimm(self):
+        self.emu.acc = self.emu.acc ^ (self.emu.instr_mem[self.emu.pc+1] & 0x0F)
+        self.emu.pc += 2
+
+    def orimm(self):
+        self.emu.acc = self.emu.acc | (self.emu.instr_mem[self.emu.pc+1] & 0x0F)
+        self.emu.pc += 2
+
+    def r4imm(self):
+        self.emu.reg[4] = self.emu.instr_mem[self.emu.pc+1] & 0x0F
+        self.emu.pc += 2
+
+    def rarbimm(self):
+        self.emu.reg[0] = self.emu.instr_mem[self.emu.pc] & 0x0F
+        self.emu.reg[1] = self.emu.instr_mem[self.emu.pc+1] & 0x0F
+        self.emu.pc += 2
+
+    def rcrdimm(self):
+        self.emu.reg[2] = self.emu.instr_mem[self.emu.pc] & 0x0F
+        self.emu.reg[3] = self.emu.instr_mem[self.emu.pc+1] & 0x0F
+        self.emu.pc += 2
+
+    def accimm(self):
+        self.emu.acc = self.emu.instr_mem[self.emu.pc] & 0x0F
+        self.emu.pc += 1
+
+    def branch(self):
+        imm = ((self.emu.instr_mem[self.emu.pc] & 0x07) << 8) | self.emu.instr_mem[self.emu.pc+1]
+        self.emu.pc = (self.emu.pc & 0xF800) | imm
+
+    def bbitkimm(self):
+        kk = (self.emu.instr_mem[self.emu.pc] & 0x18) >> 3
+        accbitkk = (self.emu.acc >> kk) & 0x1
+        if accbitkk == 1:
+            self.branch()
+
+    def bnzaimm(self):
+        if self.emu.reg[0] != 0: # RA is nonzero
+            self.branch()
+
+    def bnzbimm(self):
+        if self.emu.reg[1] != 0: # RB is nonzero
+            self.branch()
+
+    def beqzimm(self):
+        if self.emu.acc == 0:
+            self.branch()
+
+    def bnezimm(self):
+        if self.emu.acc != 0:
+            self.branch()
+
+    def beqzcfimm(self):
+        if self.emu.cf == 0:
+            self.branch()
+
+    def bnezcfimm(self):
+        if self.emu.cf != 0:
+            self.branch()
+
+    def bnzdimm(self):
+        if self.emu.reg[3] != 0:
+            self.branch()
+
+    def bimm(self):
+        imm = ((self.emu.instr_mem[self.emu.pc] & 0x0F) << 8) | self.emu.instr_mem[self.emu.pc+1]
+        self.emu.pc = (self.emu.pc & 0xF000) | imm
+
+    def callimm(self):
+        self.emu.temp = self.emu.pc + 2
+        self.bimm()
 
 if __name__ == "__main__":
     pass
